df3f60cee6fa36ca4cc6c193ad9ba69b18a9cdd1
Gregor <gregor@martynus.net>
2016-09-09 22:28:04 -0700

    test(store): always pass options.PouchDB

--- test/unit/config/store-test.js
+++ test/unit/config/store-test.js
@@ -6,43 +6,19 @@ var storeConfig = proxyquire('../../../lib/config/store', {
   './pre-auth-hook': preAuthHookStub
 })
 
-test('store config', function (group) {
-  group.test('with config.db.url = http://foo:bar@baz.com', function (group) {
-    storeConfig({
-      db: {
-        options: {
-          prefix: 'http://foo:bar@baz.com'
-        }
-      },
-      config: {
-        store: {}
-      }
-    }, function (error, config) {
-      group.error(error)
+test('store config', function (t) {
+  storeConfig({
+    db: {},
+    config: {
+      PouchDB: 'PouchDB',
+      store: {}
+    }
+  }, function (error, config) {
+    t.error(error)
 
-      group.is(config.store.couchdb, 'http://foo:bar@baz.com/', 'sets config.store.couchdb')
-      group.is(config.store.PouchDB, undefined, 'does not set config.store.PouchDB')
+    t.is(config.store.PouchDB, 'PouchDB', 'sets config.store.PouchDB')
+    t.same(config.store.hooks.onPreAuth, preAuthHookStub, 'sets pre auth hook')
 
-      group.end()
-    })
+    t.end()
   })
-
-  group.test('without config.db.url', function (group) {
-    storeConfig({
-      db: {},
-      config: {
-        PouchDB: 'PouchDB',
-        store: {}
-      }
-    }, function (error, config) {
-      group.error(error)
-
-      group.is(config.store.PouchDB, 'PouchDB', 'sets config.store.PouchDB')
-      group.is(config.store.couchdb, undefined, 'does not set config.store.couchdb')
-
-      group.end()
-    })
-  })
-
-  group.end()
 })
