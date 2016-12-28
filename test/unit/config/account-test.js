37b9137700b6d73accbb0f82e2f43928fc0c8e16
Gregor <gregor@martynus.net>
2016-12-27 17:16:25 -0800

    test: adminPassword option

--- test/unit/config/account-test.js
+++ test/unit/config/account-test.js
@@ -3,20 +3,19 @@ var test = require('tap').test
 var accountConfig = require('../../../lib/config/account')
 
 test('account config', function (t) {
-  accountConfig({
-    db: {
-      admins: 'db admins',
-      secret: 'db secret'
-    },
+  var state = {
+    admins: 'admins',
+    secret: 'secret',
     config: {
       PouchDB: 'PouchDB',
       account: {}
     }
-  }, function (error, config) {
+  }
+  accountConfig(state, function (error, config) {
     t.error(error)
 
-    t.is(config.account.admins, 'db admins', 'sets config.account.admins')
-    t.is(config.account.secret, 'db secret', 'sets config.account.secret')
+    t.is(config.account.admins, 'admins', 'sets config.account.admins')
+    t.is(config.account.secret, 'secret', 'sets config.account.secret')
     t.is(config.account.usersDb, '_users', 'sets config.account.usersDb')
     t.is(config.account.PouchDB, 'PouchDB', 'sets config.account.PouchDB')
 
--- test/unit/config/store-pre-auth-hook-test.js
+++ test/unit/config/store-pre-auth-hook-test.js
@@ -13,23 +13,6 @@ var preAuthHook = proxyquire('../../../lib/config/store/pre-auth-hook', {
 })
 
 test('store pre auth hook', function (t) {
-  var session = {
-    session: {
-      id: 'session123'
-    },
-    account: {
-      id: 'user123',
-      roles: []
-    }
-  }
-  var findSessionStub = simple.stub().returnWith({ // don’t use resolveWith to avoid async
-    then: function (callback) {
-      callback(session)
-      return {
-        catch: function () {}
-      }
-    }
-  })
   var hasAccessStub = simple.stub().returnWith({ // don’t use resolveWith to avoid async
     then: function (callback) {
       callback(true)
@@ -38,14 +21,8 @@ test('store pre auth hook', function (t) {
   })
 
   var serverStub = {
+    log: simple.stub(),
     plugins: {
-      account: {
-        api: {
-          sessions: {
-            find: findSessionStub
-          }
-        }
-      },
       store: {
         api: {
           hasAccess: hasAccessStub
@@ -123,10 +100,12 @@ test('store pre auth hook root path', function (t) {
   t.end()
 })
 
+// TODO: this test causes a UnhandledPromiseRejectionWarning and I dunno why
 test('store pre auth hook no authorization header', function (t) {
   var findSessionStub = simple.stub().rejectWith({status: 404})
   var hasAccessStub = simple.stub().resolveWith(false)
   var serverStub = {
+    log: simple.stub(),
     plugins: {
       account: {
         api: {
@@ -161,6 +140,7 @@ test('store pre auth hook session not found error', function (t) {
   var findSessionStub = simple.stub().rejectWith({status: 404})
   var hasAccessStub = simple.stub().resolveWith(false)
   var serverStub = {
+    log: simple.stub(),
     plugins: {
       account: {
         api: {
@@ -209,6 +189,7 @@ test('store pre auth hook not public access & session found', function (t) {
     return Promise.resolve(false) // not public access
   })
   var serverStub = {
+    log: simple.stub(),
     plugins: {
       account: {
         api: {
@@ -258,6 +239,7 @@ test('store pre auth hook read-only byy users for POST db/_all_docs', function (
     return Promise.resolve(false) // not public access
   })
   var serverStub = {
+    log: simple.stub(),
     plugins: {
       account: {
         api: {
@@ -305,6 +287,7 @@ test('store pre auth hook unauthorized error', function (t) {
   var findSessionStub = simple.stub().resolveWith(session)
   var hasAccessStub = simple.stub().resolveWith(false)
   var serverStub = {
+    log: simple.stub(),
     plugins: {
       account: {
         api: {
